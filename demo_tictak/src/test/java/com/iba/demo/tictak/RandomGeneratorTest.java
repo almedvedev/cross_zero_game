3fc3784f6c60ad08c3c9614d5c78b3bcacf9879e
cmd <cmdexe.by@gmail.com>
2017-07-13 15:23:58 +0300

    added the generation of random numbers in the range and test

---
+++ demo_tictak/src/test/java/com/iba/demo/tictak/RandomGeneratorTest.java
@@ -0,0 +1,77 @@
+package com.iba.demo.tictak;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.iba.demo.tictactoe.util.RandomGenerator;
+
+public class RandomGeneratorTest {
+	
+	public static final Integer MAX_ITER_COUNT = 100;
+	private RandomGenerator gen;
+	
+	@Before
+	public void init() {
+		gen = new RandomGenerator();
+	}
+	
+	@Test
+	public void testGenerationFromOneToNine() {
+		List<Integer> validatedSeq = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		Set<Integer> seq = new HashSet<>();
+		Boolean completed = false;
+		Integer iterCounter = 0;
+		
+		while(!completed) {
+			Integer num = gen.getNext(10);
+			assertNotEquals("The generated value should not be 0!", new Integer(0), num);
+
+			seq.add(num);
+			if(seq.containsAll(validatedSeq)) {
+				completed = true;
+			}
+			
+			assertNotEquals("The generator has exceeded the maximum number of iterations!",
+					MAX_ITER_COUNT, iterCounter);
+			iterCounter++;
+		}
+	}
+	
+	@Test
+	public void testGenerationInRange() {
+		Integer from = 1;
+		Integer to = 9;
+		
+		List<Integer> validatedSeq = new ArrayList<>();
+		Set<Integer> seq = new HashSet<>();
+		Boolean completed = false;
+		Integer iterCounter = 0;
+		
+		
+		for(int i = from; i < to + 1; i++) {
+			validatedSeq.add(i);
+		}
+		
+		while(!completed) {
+			Integer num = gen.getRandomNumInRange(from, to);
+			
+			seq.add(num);
+			if(seq.containsAll(validatedSeq)) {
+				completed = true;
+			}
+			
+			assertNotEquals("The generator has exceeded the maximum number of iterations!",
+					MAX_ITER_COUNT, iterCounter);
+			iterCounter++;
+		}
+	}
+	
+}
