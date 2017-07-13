e59367f70b7e635d36bbf64017a5af40954c53f1
Tatarenko_V <Tatarenko_V@tatarenko-v.iba>
2017-07-13 19:50:13 +0300

    Bot random turn

--- demo_tictak/src/main/java/com/iba/demo/tictak/service/impl/BotServiceImpl.java
+++ demo_tictak/src/main/java/com/iba/demo/tictak/service/impl/BotServiceImpl.java
@@ -1,24 +1,39 @@
 package com.iba.demo.tictak.service.impl;
 
+import com.iba.demo.tictak.model.Board;
 import com.iba.demo.tictak.model.BoardCell;
 import com.iba.demo.tictak.model.BoardCellState;
 import com.iba.demo.tictak.model.Game;
 import com.iba.demo.tictak.model.Turn;
 import com.iba.demo.tictak.service.BotService;
+import com.iba.demo.tictak.util.RandomGenerator;
+
 
 public class BotServiceImpl implements BotService {
 	
 	/* (non-Javadoc)
-	 * @see com.iba.demo.tictak.web.application.service.impl.BotService#makeRandomTurn(com.iba.demo.tictactoe.model.Game)
+	 * @see com.iba.demo.tictactoe.service.impl.BotService#makeRandomTurn(com.iba.demo.tictactoe.model.Game)
 	 */
 	@Override
 	public Turn makeRandomTurn(Game game) {
-		// TODO: by Victor
-		Turn turn = null;
-		BoardCell cell = game.getBoard().getCell(0, 0);
-		BoardCellState state = cell.getState();
-		
-		return turn;
+		if (game.getRemainingTurns() > 0) {
+			int turnNumRnd = new RandomGenerator().getRandomNumInRange(0, game.getRemainingTurns() - 1);
+
+			int freeCellNum = 0;
+			Board board = game.getBoard();
+			BoardCell cell;
+
+			for (int x = 0; x < Board.SIZE; x++) {
+				for (int y = 0; y < Board.SIZE; y++) {
+					cell = board.getCell(x, y);
+					if (BoardCellState.EMPTY.equals(cell.getState()) && freeCellNum++ == turnNumRnd) {
+						return new Turn(game.getTurnTaker(), cell);
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 }
+
